9f23e5b34daf89991a92c1267709a3ca3a99fd16
Alexey Duryagin <aiduryagih@gmail.com>
2021-09-20 08:51:11 +0300

    0.0.25

--- dist/stochastic.js
+++ dist/stochastic.js
@@ -11,13 +11,14 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     exports.Stochastic = void 0;
     const sma_1 = require("./sma");
-    function Stochastic({ candles, signalPeriod = 3, period = 14, }) {
+    function Stochastic({ candles, signalPeriod = 3, kSmoothing = 1, period = 14, }) {
         const crossResult = [];
         const result = new Map();
         const sma = sma_1.SMA({ candles: [], period: signalPeriod });
+        const kSma = sma_1.SMA({ candles: [], period: kSmoothing });
         const pastHighPeriods = [];
         const pastLowPeriods = [];
-        let k;
+        let smoothedK;
         let d;
         let index = 1;
         let lastCandle;
@@ -29,20 +30,23 @@
             if (index >= period) {
                 const periodLow = Math.min(...pastLowPeriods.slice(-period));
                 const periodHigh = Math.max(...pastHighPeriods.slice(-period));
-                k = ((candle.close - periodLow) / (periodHigh - periodLow)) * 100;
+                let pureK = ((candle.close - periodLow) / (periodHigh - periodLow)) * 100;
                 // eslint-disable-next-line no-restricted-globals
-                k = isNaN(k) ? 0 : k;
-                d = (_a = sma.update({ time: candle.time, close: k })) === null || _a === void 0 ? void 0 : _a.value;
+                pureK = isNaN(pureK) ? 0 : pureK;
+                smoothedK = kSma.update({ close: pureK, time: candle.time });
+                if (!smoothedK)
+                    return;
+                d = (_a = sma.update({ time: candle.time, close: smoothedK.value })) === null || _a === void 0 ? void 0 : _a.value;
                 // check cross
                 let cross = null;
                 if (result.size >= 1) {
                     const prevResult = Array.from(result.values()).pop();
                     const shortStoch = prevResult.d >= 80 && d < 80;
                     const longStoch = prevResult.d <= 20 && d > 20;
-                    const shortKD = prevResult.k > prevResult.d && d >= k;
-                    const longKD = prevResult.k < prevResult.d && d <= k;
-                    const shortStochKD = k >= 80 && shortKD;
-                    const longStochKD = k <= 20 && longKD;
+                    const shortKD = prevResult.k > prevResult.d && d >= smoothedK.value;
+                    const longKD = prevResult.k < prevResult.d && d <= smoothedK.value;
+                    const shortStochKD = smoothedK.value >= 80 && shortKD;
+                    const longStochKD = smoothedK.value <= 20 && longKD;
                     if (shortStoch ||
                         longStoch ||
                         shortKD ||
@@ -62,7 +66,7 @@
                         crossResult.push(cross);
                     }
                 }
-                return { k, d, time: candle.time, cross };
+                return { k: smoothedK.value, d, time: candle.time, cross };
             }
             index += 1;
             return undefined;
