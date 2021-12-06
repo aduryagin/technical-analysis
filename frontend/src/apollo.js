d4af673df2b83ac45653c37c69aa3942e86aeea8
Alexey Duryagin <aiduryagih@gmail.com>
2021-12-06 17:48:18 +0300

    fe init

---
+++ frontend/src/apollo.js
@@ -0,0 +1,15 @@
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+} from '@apollo/client';
+import config from './config';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: config.graphql,
+  }),
+});
+
+export default client;
---
+++ frontend/src/config.js
@@ -0,0 +1,8 @@
+const HOST = 'http://localhost:3001'
+
+const config = {
+  graphql: `${HOST}/graphql`,
+  socket: HOST,
+};
+
+export default config;
