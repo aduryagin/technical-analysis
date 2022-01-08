455093a771464aa11131b97b7a197f9f0b70229e
Alexey Duryagin <aiduryagih@gmail.com>
2022-01-08 19:05:03 +0300

    pmax settings

--- frontend/src/pages/chart/indicatorsTemplates/pmax.ts
+++ frontend/src/pages/chart/indicatorsTemplates/pmax.ts
@@ -2,33 +2,59 @@ import { TechnicalIndicatorTemplate } from "klinecharts";
 import { PMax } from "@aduryagin/technical-indicators";
 
 const pmaxIndicatorTemplate: TechnicalIndicatorTemplate = {
-  calcParams: [10, 10, { value: 3, allowDecimal: true }],
+  calcParams: [
+    { value: 1, allowDecimal: true },
+    { value: 3, allowDecimal: true },
+    { value: 5, allowDecimal: true },
+  ],
 
   name: "PMAX",
   series: "normal",
   plots: [
     {
-      key: "pmax",
-      title: "PMax",
+      key: "pmax1",
+      title: "PMax 1",
       type: "line",
     },
     {
-      key: "ema",
-      title: "EMA",
+      key: "pmax3",
+      title: "PMax 3",
+      type: "line",
+    },
+    {
+      key: "pmax5",
+      title: "PMax 5",
       type: "line",
     },
   ],
 
+  regeneratePlots: (params) => {
+    return params.map((p) => {
+      return { key: `pmax${p}`, title: `PMax ${p}: `, type: "line" };
+    });
+  },
+
   // Calculation results
   calcTechnicalIndicator: (kLineDataList: any, { params, plots }: any) => {
-    const data = PMax({
-      candles: kLineDataList,
-      emaPeriod: params[0],
-      atrPeriod: params[1],
-      multiplier: params[2],
-    });
+    const data = params.reduce((accum, param) => {
+      const pmax = PMax({
+        candles: kLineDataList,
+        emaPeriod: 10,
+        atrPeriod: 10,
+        multiplier: param,
+      });
 
-    return kLineDataList.map((candle: any) => data?.result(candle.time));
+      return { ...accum, [param]: pmax };
+    }, {});
+
+    return kLineDataList.map((candle: any) => {
+      return params.reduce((accum, param) => {
+        return {
+          ...accum,
+          [`pmax${param}`]: data[param]?.result(candle.time)?.pmax,
+        };
+      }, {});
+    });
   },
 };
 
